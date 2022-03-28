2c8838ec0f898beb99529de9c0b4e65b7840d1cb
Mathias Melo <melo.mathias01@gmail.com>
2022-03-28 18:14:51 -0300

    Practica de Constantes agregada

---
+++ Constantes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PracticasCSharp
+{
+    class Constantes
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.1416;
+
+            Console.WriteLine("Introduce la medida del radio:");
+
+            double radio = double.Parse(Console.ReadLine());
+
+            //double area = radio * radio * PI
+            //    ||
+            double area = Math.Pow(radio, 2) * PI;
+
+            Console.WriteLine($"El área del circulo es: {area}");
+
+        }
+    }
+
+}
+
