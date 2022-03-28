7f0ffe0cd287e9ae63e56396b4ac383253b962e3
Mathias Melo <melo.mathias01@gmail.com>
2022-03-28 17:43:59 -0300

    Practicas del Curso de CSharp Pildoras Informaticas

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PracticasCSharp
+{ 
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            int primerNumero;
+            int segundoNumero;
+
+            Console.WriteLine("Introduce primer numero: ");
+            primerNumero = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce segundo numero: ");
+            segundoNumero = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("El resultado de " + primerNumero +" + "+ segundoNumero + " es: "+(primerNumero+segundoNumero));
+            Console.WriteLine("El resultado de " + primerNumero + " - " + segundoNumero + " es: " + (primerNumero - segundoNumero));
+            Console.WriteLine("El resultado de " + primerNumero + " * " + segundoNumero + " es: " + (primerNumero * segundoNumero));
+            double primerNumeroDecimal = Convert.ToDouble(primerNumero);
+            double segundoNumeroDecimal = Convert.ToDouble(segundoNumero);
+            Console.WriteLine("El resultado de " + primerNumeroDecimal + " / " + segundoNumeroDecimal + " es: " + (primerNumeroDecimal / segundoNumeroDecimal));
+             
+        }
+    }
+
+}
+ci
